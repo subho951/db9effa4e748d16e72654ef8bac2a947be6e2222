d36999b8a043e7108b9449c5f4f91137f05fb35f
subhomoy samanta <subhomoy@keylines.net>
2024-11-20 00:20:36 +0530

    html folders uploaded

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/env-test', function () {
+    return env('APP_NAME', 'Default Name');
+});
 /* Front Panel */
     // before login
         Route::match(['get', 'post'], '/', 'App\Http\Controllers\FrontController@home');

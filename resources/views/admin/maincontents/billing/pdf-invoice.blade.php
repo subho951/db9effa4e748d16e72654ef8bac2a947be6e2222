85eb37f0dcb5342ae17ee7b41964242e5f058b6c
surajit bera <surajit.b@keylines.net>
2025-03-23 17:34:53 +0530

    pdf design

--- resources/views/admin/maincontents/billing/pdf-invoice.blade.php
+++ resources/views/admin/maincontents/billing/pdf-invoice.blade.php
@@ -7,20 +7,45 @@ use App\Helpers\Helper;
 $generalSetting = GeneralSetting::find(1);
 ?>
 <title><?=$generalSetting->site_name?>-Invoice-<?=$getOrderDetail->order_no?></title>
+<style>
+   *{
+      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      
+   }
+   .border {
+      border: 1px solid rgb(108,117,125);
+   }
+   .border-secondary {
+      --bs-border-opacity: 1;
+      border-color: rgba(var(--bs-secondary-rgb), var(--bs-border-opacity)) !important;
+   }
+   .invoice_heading{
+      text-align: center;
+      font-size: 1.75rem;
+      line-height: 1;
+   }
+    
+   td{
+      vertical-align: top;
+      padding: 10px;
+      border-bottom: 1px solid #000;
+      font-size: 15px;
+   }
+</style>
 <!-- Favicon -->
 <link rel="icon" type="image/x-icon" href="<?=env('UPLOADS_URL')?><?=$generalSetting->site_favicon?>" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-<div class="container-fluid invoice-container" style="margin: 15px auto;padding: 40px;max-width: 850px;background-color: #fff;border: 1px solid #ccc;-moz-border-radius: 6px;-webkit-border-radius: 6px;-o-border-radius: 6px;border-radius: 6px;">
-   <table class="table table-bordered border border-secondary mb-0">
+<div style="border: 1px solid rgb(108,117,125);">
+   <table class="table" style="width: 100% !important; border-collapse: collapse;">
       <tbody>
          <tr>
-            <td colspan="2" class="bg-light text-center">
-               <h3 class="mb-0"><strong><?=$generalSetting->site_name?></strong></h3>
-               <h3 class="mb-0"><strong>Invoice</strong></h3>
+            <td colspan="2" class="bg-light text-center" style="background-color: #f8f9fa; padding: 0; border-bottom: 1px solid rgb(108,117,125);">
+               <h3 class="invoice_heading"><strong><?=$generalSetting->site_name?><br/> Invoice</strong></h3>
+               <!-- <h3 class="mb-0"><strong>Invoice</strong></h3> -->
             </td>
          </tr>
          <tr>
-            <td class="col-7">
+            <td style="width:60%; border-bottom: 1px solid rgb(108,117,125);">
                <div class="row gx-2 gy-2">
                   <div><strong>Delivery Mode : </strong><?=$getOrderDetail->delivery_mode?></div>
                   <?php if($getOrderDetail->delivery_mode != 'Take'){?>
@@ -47,32 +72,26 @@ $generalSetting = GeneralSetting::find(1);
                   <?php }?>
                </div>
             </td>
-            <td class="col-5 bg-light">
-               <div class="row gx-2 gy-1 fw-600">
-                  <div class="col-5">Invoice No <span class="float-end">:</span></div>
-                  <div class="col-7">#<?=$getOrderDetail->order_no?></div>
-                  <div class="col-5">Date <span class="float-end">:</span></div>
-                  <div class="col-7"><?=date_format(date_create($getOrderDetail->order_date), "M d, Y")?> <?=date_format(date_create($getOrderDetail->order_time), "h:i A")?></div>
-
-                  <div class="col-5">Payment Status <span class="float-end">:</span></div>
-                  <div class="col-7" style="font-size: 11px;"><?=(($getOrderDetail->payment_status)?'SUCCESS':'FAILED')?></div>
-                  <div class="col-5">Payment Mode <span class="float-end">:</span></div>
-                  <div class="col-7" style="font-size: 11px;"><?=$getOrderDetail->payment_mode?></div>
-                  <div class="col-5">Date/Time <span class="float-end">:</span></div>
-                  <div class="col-7" style="font-size: 11px;"><?=date_format(date_create($getOrderDetail->payment_date_time), "M d, Y h:i A")?></div>
+            <td style="width:40%; border-bottom: 1px solid rgb(108,117,125); background-color: #f8f9fa; border-left: 1px solid rgb(108,117,125);">
+               <div class="row gx-2 gy-1">
+                  <div style="margin: 7px 0;"><span style="width: 110px; display: inline-block;">Invoice No</span>: #<?=$getOrderDetail->order_no?></div>
+                  <div style="margin: 7px 0;"><span style="width: 110px; display: inline-block;">Date</span>: <code style="font-size: 13px; "><?=date_format(date_create($getOrderDetail->order_date), "M d, Y")?> <?=date_format(date_create($getOrderDetail->order_time), "h:i A")?></code></div>
+                  <div style="margin: 7px 0;"><span style="width: 110px; display: inline-block;">Payment Status</span> <code style="font-size: 12px; margin-top: -5px; display: inline-block;">: <?=(($getOrderDetail->payment_status)?'SUCCESS':'FAILED')?></code></div>
+                  <div style="margin: 7px 0;"><span style="width: 110px; display: inline-block;">Payment Mode</span> <code style="font-size: 12px; margin-top: -5px; display: inline-block;">: <?=$getOrderDetail->payment_mode?></code></div>
+                  <div style="margin: 7px 0;"><span style="width: 110px; display: inline-block;">Date/Time</span> <code style="font-size: 12px; margin-top: -5px; display: inline-block;">: <?=date_format(date_create($getOrderDetail->payment_date_time), "M d, Y h:i A")?></code></div>
                </div>
             </td>
          </tr>
          <tr>
-            <td colspan="2" class="p-0">
-               <table class="table table-sm mb-0">
+            <td colspan="2" style="padding: 0;">
+               <table class="table" style="width: 100% !important; border-collapse: collapse;">
                   <thead>
                      <tr class="bg-light">
-                        <td class="col-1 text-center"><strong>#</strong></td>
-                        <td class="col-6 "><strong>Item Name</strong></td>
-                        <td class="col-1 text-center"><strong>Qty</strong></td>
-                        <td class="col-2 text-end"><strong>Price</strong></td>
-                        <td class="col-2 text-end"><strong>Amount</strong></td>
+                        <td style="width: 10%;"><strong>#</strong></td>
+                        <td style="width: 55%;"><strong>Item Name</strong></td>
+                        <td style="width: 15%;"><strong>Qty</strong></td>
+                        <td style="width: 15%; text-align: right;"><strong>Price</strong></td>
+                        <td style="width: 15%; text-align: right;"><strong>Amount</strong></td>
                      </tr>
                   </thead>
                   <tbody>
@@ -88,56 +107,51 @@ $generalSetting = GeneralSetting::find(1);
                            <td class="col-1 text-center"><?=$sl++?></td>
                            <td class="col-6"><?=$getProduct->name?></td>
                            <td class="col-1 text-center"><?=$orderDetail->qty?></td>
-                           <td class="col-2 text-end">$<?=number_format($orderDetail->price,2)?></td>
-                           <td class="col-2 text-end">$<?=number_format($orderDetail->subtotal,2)?></td>
+                           <td style="text-align: right;">$<?=number_format($orderDetail->price,2)?></td>
+                           <td style="text-align: right;">$<?=number_format($orderDetail->subtotal,2)?></td>
                         </tr>
                      <?php } }?>
                   </tbody>
                </table>
             </td>
          </tr>
-         <tr class="bg-light fw-600">
-            <td class="col-7 py-1"></td>
-            <td class="col-5 py-1 pe-1">Sub Total: <span class="float-end">$<?=number_format($subtotal,2)?></span></td>
-         </tr>
-         <tr class="bg-light fw-600">
-            <td class="col-7 py-1"></td>
-            <td class="col-5 py-1 pe-1">Discount: <span class="float-end">$<?=number_format($getOrderDetail->discount_amount,2)?></span></td>
-         </tr>
-         <tr class="bg-light fw-600">
-            <td class="col-7 py-1"></td>
-            <td class="col-5 py-1 pe-1">Shipping: <span class="float-end">$<?=number_format($getOrderDetail->delivery_amount,2)?></span></td>
-         </tr>
-         <tr class="bg-light fw-600">
-            <td class="col-7 py-1"></td>
-            <td class="col-5 py-1 pe-1">Net Total: <span class="float-end">$<?=number_format($getOrderDetail->net_amount,2)?></span></td>
-         </tr>
-         <tr class="bg-light fw-600">
-            <td class="col-7 py-1"></td>
-            <td class="col-5 py-1 pe-1">Amount in Words: <span class="float-start"><i><?=Helper::getIndianCurrency($getOrderDetail->net_amount)?></i></span></td>
+         <tr>
+            <td style="width: 60%"></td>
+            <td style="width: 40%; border-left: 1px solid #000;">Sub Total: <span style="float: right">$<?=number_format($subtotal,2)?></span></td>
          </tr>
          <tr>
-            <td class="col-7 text-1">
+            <td style="width: 60%"></td>
+            <td style="width: 40%; border-left: 1px solid #000;">Discount: <span style="float: right">$<?=number_format($getOrderDetail->discount_amount,2)?></span></td>
+         </tr>
+         <tr>
+            <td style="width: 60%"></td>
+            <td style="width: 40%; border-left: 1px solid #000;">Shipping: <span style="float: right">$<?=number_format($getOrderDetail->delivery_amount,2)?></span></td>
+         </tr>
+         <tr>
+            <td style="width: 60%"></td>
+            <td style="width: 40%; border-left: 1px solid #000;">Net Total: <span style="float: right">$<?=number_format($getOrderDetail->net_amount,2)?></span></td>
+         </tr>
+         <tr>
+            <td style="width: 60%"></td>
+            <td style="width: 40%; border-left: 1px solid #000;">Amount in Words: <span class="float-start"><br><i><?=Helper::getIndianCurrency($getOrderDetail->net_amount)?></i></span></td>
+         </tr>
+         <tr>
+            <td rowspan="2" colspan="1" style="border: 1px solid #000; border-left: 0;">
                <?=$getOrderDetail->note?>
             </td>
-            <td class="col-5 pe-1 text-end" style="padding: 0;">
-               <div class="text-1 mt-5" style="border-top: 1px solid #6c757d;text-align: center;">Authorized Signature <br>For <?=$generalSetting->site_name?></div>
-            </td>
+            <td colspan="1" style="height: 30px;"></td>
          </tr>
          <tr>
-            <td colspan="3" class="p-0">
-               <table class="table table-sm mb-0 table-bordered">
-                  <tr>
-                     <td class="col-12 pe-1">
-                        This is computer generated invoice does not require signature
-                     </td>
-                  </tr>
-               </table>
+            <td style="text-align: center;">Authorized Signature <br>For <?=$generalSetting->site_name?></td>
+         </tr>
+         <tr>
+            <td colspan="2">
+               This is computer generated invoice does not require signature
             </td>
          </tr>
       </tbody>
    </table>
+</div>
    <!-- <footer class="text-center mt-4">
       <div class="btn-group btn-group-sm d-print-none"> <a href="javascript:window.print()" class="btn btn-light border text-black-50 shadow-none"><i class="fa fa-print"></i> Print & Download</a> </div>
    </footer> -->
-</div>
